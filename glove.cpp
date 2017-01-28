f02d1047fc344de7a4d8a2c192bbf6ec7d52bbf0
ni-cc <ni-cc@users.noreply.github.com>
2017-01-28 20:24:45 +0100

    add states, more bluetooth communication

--- glove.cpp
+++ glove.cpp
@@ -1,27 +1,82 @@
+#include <functional>
 #include <QSharedPointer>
+#include <QVariant>
 #include <QBluetoothDeviceInfo>
 #include <QBluetoothSocket>
 #include <QBluetoothUuid>
-#include "extern-plugininfo.h"
+//#include "extern-plugininfo.h"
 #include "glove.h"
-
-Glove::Glove(const QBluetoothDeviceInfo &leftInfo, const QBluetoothDeviceInfo &rightInfo, QObject *parent) :
-    QObject(parent)
-{
-    m_sockets.insert(leftInfo, QSharedPointer<QBluetoothSocket>());
-    m_sockets.insert(rightInfo, QSharedPointer<QBluetoothSocket>());
+//#include <iostream>
+Glove::Glove(const QString &name, const QString &leftMAC, const QString &rightMAC,
+             const std::function<bool()> &isRecording, QObject *parent) :
+    QObject(parent), m_isRecording(isRecording)
+{   
+    auto leftAddress = QBluetoothAddress(leftMAC);
+    auto rightAddress = QBluetoothAddress(rightMAC);
+    auto leftInfo = QBluetoothDeviceInfo(leftAddress, name, 0);
+    auto rightInfo = QBluetoothDeviceInfo(rightAddress, name, 0);
+    m_connections.insert(leftInfo, QSharedPointer<QBluetoothSocket>());
+    m_connections.insert(rightInfo, QSharedPointer<QBluetoothSocket>());
 }
 
-void Glove::connect()
+void Glove::connectDevice()
 {
-    foreach (const auto deviceInfo, m_sockets.keys()) {
+    foreach (const auto deviceInfo, m_connections.keys()) {
         QSharedPointer<QBluetoothSocket> socket{new QBluetoothSocket(QBluetoothServiceInfo::RfcommProtocol)};
-        m_sockets[deviceInfo] = socket;
+        connect(socket.data(), &QBluetoothSocket::connected, this, &Glove::emitConnectionChanged);
+        connect(socket.data(), &QBluetoothSocket::disconnected, this, &Glove::emitConnectionChanged);
+        connect(socket.data(), &QBluetoothSocket::readyRead, this, [deviceInfo, this](){ read(deviceInfo); });
+
+        m_connections[deviceInfo] = socket;
     }
-    for (auto it = m_sockets.begin(); it != m_sockets.end(); it++) {
-        it.value()->connectToService(it.key().address(), QBluetoothUuid(QBluetoothUuid::SerialPort));
-        //connect(it.value(), &QBluetoothSocket::connected, this, ());
+
+    for (auto device = m_connections.begin(); device != m_connections.end(); device++) {
+        device.value()->connectToService(device.key().address(), QBluetoothUuid(QBluetoothUuid::SerialPort));
     }
+}
+
+void Glove::read(const QBluetoothDeviceInfo deviceInfo)
+{
+    /*
+    struct message {
+        accelerationX;
+        rotationX;
+        rfid;
+        grasp;
+        userInputButton;
+        handIsinGlove;
+        additionalButton;
+        isSameRFIDTag;
+    } dataPoint;
+*/
+    if (!m_isRecording()) {
+        m_connections[deviceInfo]->readAll();
+        return;
+    }
+    //std::cerr << "a" << std::endl;
+    char c;
+    auto &packet = m_packets[deviceInfo];
+    while (m_connections[deviceInfo]->getChar(&c)) {
+        if (c == '\0') {
+            //cobs_decode(packet.data(), packet.size(), &dataPoint);
+            //db
+            packet.clear();
+        } else {
+            packet += c;
+        }
+    }
+     //   std::cerr.write(&c, 1);
+    //std::cerr << std::endl;
+    //emit, continue
+}
+
+void Glove::emitConnectionChanged() {
+    Connected state = Connected::none;
+    if (m_connections.begin().value()->state() == QBluetoothSocket::ConnectedState)
+        state = Connected::left;
+    if ((++m_connections.begin()).value()->state() == QBluetoothSocket::ConnectedState)
+        state = static_cast<Connected>(state & Connected::right);
+    emit connectionChanged(state);
 }
 
 inline uint qHash(const QBluetoothDeviceInfo &info) {
--- glove.h
+++ glove.h
@@ -1,21 +1,38 @@
 #ifndef GLOVE_H
 #define GLOVE_H
 
+#include <functional>
 #include <QHash>
 #include <QSharedPointer>
+#include <QVariant>
 #include <QBluetoothDeviceInfo>
 #include <QBluetoothSocket>
-#include "extern-plugininfo.h"
+//#include "extern-plugininfo.h"
 
 class Glove : public QObject
 {
     Q_OBJECT
 public:
-    explicit Glove(const QBluetoothDeviceInfo &leftInfo, const QBluetoothDeviceInfo &rightInfo, QObject *parent = 0);
-    void connect();
+    explicit Glove(const QString &name, const QString &leftMAC, const QString &rightMAC,
+                   const std::function<bool()> &isRecording, QObject *parent = 0);
+    void connectDevice();
+
+    enum Connected { none, left, right, both };
+    Q_ENUM(Connected)
+
+signals:
+    void connectionChanged(QVariant state);
 
 private:
-    QHash<QBluetoothDeviceInfo, QSharedPointer<QBluetoothSocket>> m_sockets;
+    void read(const QBluetoothDeviceInfo deviceInfo);
+
+    std::function<bool()> m_isRecording;
+
+    QHash<QBluetoothDeviceInfo, QSharedPointer<QBluetoothSocket>> m_connections;
+    QHash<QBluetoothDeviceInfo, QString> m_packets;
+
+private slots:
+    void emitConnectionChanged();
 };
 
 #endif // GLOVE_H
